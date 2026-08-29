95ea60823301d1d748122b8053f28fa908791a4d
Ashmita S.Sharma <ashmita.in.com@gmail.com>
2026-08-29 12:20:12 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
-import authRoutes from "./routes/auth.routes.js";
-import examRoutes from "./routes/exam.routes.js";
-import userRoutes from "./routes/user.routes.js";
+import authRoutes from './auth.routes.js';
+import examRoutes from './exam.route.js';
+import userRoutes from './user.route.js';
 import connectDB from "./config/db.js";
 
 dotenv.config();
