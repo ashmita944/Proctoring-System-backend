ddfcf8543016bc31750096970dd2c9771bd3090f
Ashmita S.Sharma <ashmita.in.com@gmail.com>
2026-09-01 13:15:40 -0400

    Update user.route.js

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getUserProfile, updateUserProfile } from "../controllers/user.controller.js";
-import authMiddleware from "../middlewares/auth.middleware.js";
+import authMiddleware from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
